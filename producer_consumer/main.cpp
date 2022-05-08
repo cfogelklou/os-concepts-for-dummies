d2f477e01b47deaf38211471d3ed5168d106b11e
Chris Fogelklou <chris.fogelklou@volvocars.com>
2022-05-08 09:16:48 +0200

    Works. Now remove the mutex and show what happens.

--- producer_consumer/main.cpp
+++ producer_consumer/main.cpp
@@ -1,31 +1,68 @@
+#include <chrono>
 #include <cstdint>
+#include <deque>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
-// Shared with the interrupt service routine
-char _rxchar = 0;
-bool _rxflag = false;
 
-static int32_t uart_rx = 0;
+using namespace std::chrono_literals;
 
-// Hardware address of the serial port
-#define UART0_RX_PTR ((volatile char*)&uart_rx)
+static volatile auto running = true;
+std::mutex lock;
 
-void foo() {
-  // do stuff...
+void producer(std::deque<uint8_t>* pfifo8) {
+  // produce stuff...
+  int32_t loops = 0;
+  while (running) {
+    {
+      std::lock_guard<std::mutex> guard(lock);
+      pfifo8->push_back((loops >> 0) & 0x000000ff);
+      // std::this_thread::sleep_for(1ms);
+      pfifo8->push_back((loops >> 8) & 0x000000ff);
+      // std::this_thread::sleep_for(1ms);
+      pfifo8->push_back((loops >> 16) & 0x000000ff);
+      // std::this_thread::sleep_for(1ms);
+      pfifo8->push_back((loops >> 24) & 0x000000ff);
+      loops++;
+    }
+    std::this_thread::sleep_for(10ms);
+  }
 }
 
-void bar(int x) {
+void consumer(std::deque<uint8_t>* pfifo8) {
   // do stuff...
-  std::cout << "Hello from bar" << std::endl;
+  std::cout << "Hello from consumer" << std::endl;
+  while (running) {
+    {
+      std::lock_guard<std::mutex> guard(lock);
+      if (!pfifo8->empty()) {
+        const int32_t x0 = pfifo8->front();
+        pfifo8->pop_front();
+        const int32_t x1 = pfifo8->front();
+        pfifo8->pop_front();
+        const int32_t x2 = pfifo8->front();
+        pfifo8->pop_front();
+        const int32_t x3 = pfifo8->front();
+        pfifo8->pop_front();
+        // std::cout << "Consumed " << x << std::endl;
+        const int32_t x = x0 | (x1 << 8) | (x2 << 16) | (x3 << 24);
+        std::cout << "Got " << x << std::endl;
+      }
+    }
+    std::this_thread::sleep_for(1ms);
+  }
 }
 
 
 int main() {
+  std::deque<uint8_t> fifo;
   // std::cout << "Hello world" << std::endl;
-  std::thread first(foo); // spawn new thread that calls foo()
-  std::thread second(bar, 1); // spawn new thread that calls foo()
+  std::thread first(producer, &fifo); // spawn new thread that calls foo()
+  std::thread second(consumer, &fifo); // spawn new thread that calls foo()
 
+  std::this_thread::sleep_for(20000ms);
+  running = false;
 
   // synchronize threads:
   first.join(); // pauses until first finishes
