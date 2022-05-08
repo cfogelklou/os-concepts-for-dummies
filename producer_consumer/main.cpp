0e6546a70eae9e0445ae0b8de5ec533f3828c4b7
Chris Fogelklou <chris.fogelklou@volvocars.com>
2022-05-08 08:43:45 +0200

    Builds

--- producer_consumer/main.cpp
+++ producer_consumer/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <thread>
 // Shared with the interrupt service routine
 char _rxchar = 0;
 bool _rxflag = false;
@@ -10,6 +11,24 @@ static int32_t uart_rx = 0;
 // Hardware address of the serial port
 #define UART0_RX_PTR ((volatile char*)&uart_rx)
 
+void foo() {
+  // do stuff...
+}
+
+void bar(int x) {
+  // do stuff...
+  std::cout << "Hello from bar" << std::endl;
+}
+
+
 int main() {
-  std::cout << "Hello world" << std::endl;
+  // std::cout << "Hello world" << std::endl;
+  std::thread first(foo); // spawn new thread that calls foo()
+  std::thread second(bar, 1); // spawn new thread that calls foo()
+
+
+  // synchronize threads:
+  first.join(); // pauses until first finishes
+  second.join();
+  return 0;
 }
